c819c65e0c9e0577ed0088c8f2fc8cff040483fa
Riaeth <drasneel@gmail.com>
2019-05-20 08:25:00 +0200

    modificado

--- src/main/java/com/example/demo/repository/PersonaRepository.java
+++ src/main/java/com/example/demo/repository/PersonaRepository.java
@@ -2,10 +2,14 @@ package com.example.demo.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import com.example.demo.model.PersonaModel;
 
 
-public interface PersonaRepository {
+public interface PersonaRepository extends JpaRepository<PersonaModel, Integer>{
 
+	@Query("SELECT DISTINCT p FROM PersonaModel p")
 	public List<PersonaModel> obtenerPersonas();
 }
